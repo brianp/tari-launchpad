489537cae841c9406c7972ad9e5892398f632093
Denis Kolodin <deniskolodin@gmail.com>
2022-10-04 17:37:11 +0200

    fix: volume events

--- sdm/src/volume/task/events.rs
+++ sdm/src/volume/task/events.rs
@@ -1,10 +1,24 @@
 use anyhow::Error;
 
-use super::{Event, VolumeTask};
+use super::{Event, Status, VolumeTask};
 use crate::{config::ManagedConfig, task::TaskContext};
 
 impl<C: ManagedConfig> TaskContext<VolumeTask<C>> {
-    pub fn process_event_impl(&mut self, _event: Event) -> Result<(), Error> {
+    pub fn process_event_impl(&mut self, event: Event) -> Result<(), Error> {
+        match event {
+            Event::Created => self.on_created(),
+            Event::Destroyed => self.on_destroyed(),
+        }
+    }
+
+    fn on_created(&mut self) -> Result<(), Error> {
+        if let Status::WaitCreating = self.status.get() {
+            self.status.set(Status::Active);
+        }
+        Ok(())
+    }
+
+    fn on_destroyed(&mut self) -> Result<(), Error> {
         Ok(())
     }
 }
