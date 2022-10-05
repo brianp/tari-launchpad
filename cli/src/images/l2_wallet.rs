dd84a396c9536112391695fa8623a8baec1677d7
brianp <brian.o.pearce@gmail.com>
2022-10-05 14:57:24 +0200

    Bootstrap the wallet sans password

--- cli/src/images/l2_wallet.rs
+++ cli/src/images/l2_wallet.rs
@@ -2,6 +2,7 @@ use tari_sdm::{
     ids::{ManagedTask, TaskId},
     image::{ManagedContainer, Ports, Volumes},
 };
+use tari_sdm::image::{Args, Envs};
 
 use super::{TariBaseNode, DEFAULT_REGISTRY, GENERAL_VOLUME};
 use crate::{
@@ -44,6 +45,36 @@ impl ManagedContainer for TariWallet {
     fn ports(&self, ports: &mut Ports) {
         ports.add(18_143);
         ports.add(18_188);
+    }
+
+    fn args(&self, args: &mut Args) {
+        self.
+        args.set("--log-config", "/var/tari/config/log4rs.yml");
+        args.set("--seed-words-file", "/var/tari/config/seed_words.txt");
+        args.flag("--enable-grpc");
+        args.flag("-n");
+
+        if let Some(settings) = self.settings.as_ref() {
+            args.set("-p", format!("wallet.custom_base_node={}::{}", "hi","hi"));
+        }
+    }
+
+    fn envs(&self, envs: &mut Envs) {
+        if let Some(settings) = self.settings.as_ref() {
+            settings.add_common(envs);
+            settings.add_tor(envs);
+            envs.set("WAIT_FOR_TOR", 10);
+            envs.set(
+                "TARI_BASE_NODE__DATA_DIR",
+                format!("/blockchain/{}", settings.tari_network.lower_case()),
+            );
+            envs.set("TARI_WALLET_PASSWORD", &settings.wallet_password); // HERE
+            envs.set("TARI_WALLET__P2P__TRANSPORT__TOR__CONTROL_AUTH", format!("password={}", &settings.tor_password));
+        }
+        envs.set("SHELL", "/bin/bash");
+        envs.set("TERM", "linux");
+        envs.set("APP_NAME", "wallet");
+        envs.set("APP_EXEC", "tari_console_wallet");
     }
 
     fn volumes(&self, volumes: &mut Volumes) {
