b81f7b8464cd717c96da548999f9816488d5745e
brianp <brian.o.pearce@gmail.com>
2022-10-07 15:35:57 +0200

    WIP

--- sdm-lp/src/images/l8_grafana.rs
+++ sdm-lp/src/images/l8_grafana.rs
@@ -1,13 +1,15 @@
 use tari_sdm::{
     ids::{ManagedTask, TaskId},
-    image::{Envs, ManagedContainer, Networks, Ports},
+    image::{Envs, ManagedContainer, Mounts, Networks, Ports, Volumes},
 };
 
 use super::GRAFANA_REGISTRY;
 use crate::{
     config::{ConnectionSettings, LaunchpadConfig, LaunchpadProtocol},
+    images::{GENERAL_VOLUME, GRAFANA_DEFAULTS_PATH, GRAFANA_PROVISION_PATH, GRAFANA_VOLUME},
     networks::LocalNet,
 };
+use crate::images::VAR_TARI_PATH;
 
 #[derive(Debug, Default)]
 pub struct Grafana {
@@ -58,9 +60,34 @@ impl ManagedContainer for Grafana {
 
     fn ports(&self, ports: &mut Ports) {
         ports.add(18_300);
+        ports.add(3000);
     }
 
     fn reconfigure(&mut self, config: Option<&LaunchpadConfig>) -> bool {
         config.map(|conf| conf.with_monitoring).unwrap_or_default()
+    }
+
+    fn volumes(&self, volumes: &mut Volumes) {
+        volumes.add(GENERAL_VOLUME);
+        volumes.add(GRAFANA_VOLUME);
+    }
+
+    fn mounts(&self, mounts: &mut Mounts) {
+        if let Some(settings) = self.settings.as_ref() {
+            // TODO: Avoid using display here
+            mounts.bind_path(settings.data_directory.display(), VAR_TARI_PATH);
+            mounts.bind_path(
+                settings.data_directory.join("config").join("defaults.ini").display(),
+                GRAFANA_DEFAULTS_PATH,
+            );
+            mounts.bind_path(
+                settings
+                    .data_directory
+                    .join("config")
+                    .join("sources_provision.yml")
+                    .display(),
+                GRAFANA_PROVISION_PATH,
+            );
+        }
     }
 }
